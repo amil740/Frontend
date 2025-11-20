83bc245b99f2186b2e6ba9fab2c8d2c08e166cb5
amil740 <amilshm@code.edu.az>
2025-11-20 17:31:15 +0400

    JS intro

---
+++ Js/assets/js/index.js
@@ -0,0 +1,44 @@
+var n = 21;
+var m = 37;
+
+if (n % 3 == 0 && n % 7 === 0) {
+    console.log("3 ve 7 e bolunur");
+}
+
+var count = 0;
+for (var i = n; i <= m; i++) {
+    if (i % 2 !== 0) {
+        count++;
+    }
+}
+console.log(count);
+
+var sum = 0;
+for (var j = n; j <= m; j++) {
+    if (j % 2 !== 0) {
+        sum = sum + j;
+    }
+}
+console.log(sum);
+
+var bolen = 0;
+
+for (var k = 1; k <= n; k++) {
+    if (n % k === 0) {
+        bolen++;
+    }
+}
+
+if (bolen == 2) {
+    console.log("eded sadedir");
+} else {
+    console.log("eded murekkebdir");
+}
+var arr = [4,22,7,32,5,8,19,12];
+var sumeven = 0;
+for (var x = 0; x < arr.length; x++) {
+        if (arr[x] % 2 == 0) {
+            sumeven += arr[x];
+        }
+    }
+console.log(sumeven);
